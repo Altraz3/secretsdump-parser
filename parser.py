43ff8fa0c1ce9f91ce9bb00ecb8cb28673b1a7db
Altraz3 <alkatraz043@gmail.com>
2026-05-18 18:13:28 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -7,12 +7,3 @@ with open(r'c:\Net\Вспомогательное\PYTHON\Projects\ntlmhashess.tx
      parts = line.strip().split(":")
      print(f'{parts[0]} : {parts[3]}')
      file.write((f'{parts[0]} : {parts[3]}\n'))
-
-    
-# (r'c:\Net\Вспомогательное\PYTHON\Projects\hashess.txt', 'r') - путь к файлу(r-чтение, w - запись, a - добавление )
-# readlines - возвращает список строк
-# for line in lines - для каждой строки из списка строк
-# parts - переменная для списка
-# strip() - удаляет добавляемое "readlines" в конце каждой строки значение \n
-# split() - после деления строка превращается в список
-# file.write - 
